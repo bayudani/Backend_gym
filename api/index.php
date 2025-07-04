d6fe55f84c5092d632b41fddc85d679d19db938f
bayudani <bayu22122017@gmail.com>
2025-07-04 15:44:43 +0700

    refactor: clean up index.php and add trusted proxies configuration in AppServiceProvider

--- api/index.php
+++ api/index.php
@@ -1,4 +1,2 @@
 <?php
-
-// Panggil file index.php publik dari Laravel
 require __DIR__ . '/../public/index.php';
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\PersonalAccessToken;
 use Laravel\Sanctum\Sanctum;
+use Illuminate\Http\Request; // <-- PASTIKAN BARIS INI ADA DI ATAS
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +29,14 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'local') {
             URL::forceScheme('https');
         };
+        // 👇 TAMBAHKAN KODE INI DI SINI 👇
+        Request::setTrustedProxies(
+            ['*'],
+            Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     }
 }
